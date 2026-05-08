2bb7b805c2748edc0d50a94427931da6ca004b25
ARST113 <96774211+ARST113@users.noreply.github.com>
2026-05-08 13:34:10 +0300

    Use shared OpenXR platform header in swapchain header

--- app/src/main/cpp/openxr/OpenXrSwapchain.h
+++ app/src/main/cpp/openxr/OpenXrSwapchain.h
@@ -1,9 +1,9 @@
 #pragma once
 
-#include <GLES3/gl3.h>
-#include <openxr/openxr.h>
-#include <openxr/openxr_platform.h>
+#include "OpenXrPlatform.h"
+
 #include <vector>
+
 class OpenXrSwapchain {
 public:
     bool create(XrSession session, int32_t width, int32_t height);
@@ -13,6 +13,7 @@ public:
     int32_t width() const { return width_; }
     int32_t height() const { return height_; }
     GLuint activeColorTexture() const;
+
 private:
     XrSwapchain swapchain_{XR_NULL_HANDLE};
     std::vector<XrSwapchainImageOpenGLESKHR> images_;
